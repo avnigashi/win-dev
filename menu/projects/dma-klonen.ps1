609bcfe444d1f30f9bf6d2fb9ec4b4bdecfa4696
Avni Gashi <avnig92@gmail.com>
2024-06-28 03:09:11 +0200

    Add files via upload

---
+++ menu/projects/dma-klonen.ps1
@@ -0,0 +1,24 @@
+function DMA-Klonen {
+    $repoUrl = "https://github.com/healexsystems/cds"
+    $branchName = "asz/dma-latest"
+    $targetDir = Read-Host "Enter the target directory (leave blank to use the current directory)"
+    
+    if (-not $targetDir) {
+        $targetDir = (Get-Location).Path
+    }
+
+    Write-Host "Cloning repository from $repoUrl into $targetDir..."
+    Show-LoadingAnimation -message "Cloning repository" -durationSeconds 10
+    try {
+        git clone --branch $branchName $repoUrl $targetDir
+        if ($LASTEXITCODE -eq 0) {
+            Write-Host "Repository cloned successfully into $targetDir."
+        } else {
+            Write-Host "Failed to clone the repository."
+        }
+    } catch {
+        Write-Host "Error cloning repository: $_"
+    }
+}
+
+DMA-Klonen
